e6fbfbf780c82355c9fa6efef5e6d32fdcac17ef
Tobias <lutobias@student.ethz.ch>
2020-10-26 12:45:56 +0100

    change dns replies

--- project/dnsserver.py
+++ project/dnsserver.py
@@ -1,15 +1,24 @@
 from dnslib.server import DNSServer, DNSRecord
 from dnslib import RR
+import sys
+
+record = ''
+
+for i, arg in enumerate(sys.argv):
+    if i == 0:
+        continue
+    if i == 1:
+        record = arg
 
 class TestResolver:
     def resolve(self,request,handler):
         reply = request.reply()
-        reply.add_answer(*RR.fromZone(". 60 IN A 127.0.0.1"))
+        reply.add_answer(*RR.fromZone(f". 60 IN A {record}"))
         print("dns ", reply)
         return reply
 
 resolver = TestResolver()
-server = DNSServer(resolver, port=10053, address="127.0.0.1")
+server = DNSServer(resolver, port=10053, address=record)
 server.start()
 
 
